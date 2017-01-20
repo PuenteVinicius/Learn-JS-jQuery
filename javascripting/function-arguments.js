fe06233a5ff8d8b096a8eebe3aae8818a6451b8c
PuenteVinicius <viniciuspuente_2@hotmail.com>
2017-01-20 16:28:48 -0200

    javascripting added

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,6 @@
+function math(a, b, c) {
+    var result;
+    result = a + (b * c);
+    console.log(result);
+}
+math(53, 61, 67);
---
+++ javascripting/functions.js
@@ -0,0 +1,4 @@
+function eat(food) {
+    return food + 'tasted rtouch eally good.';
+}
+console.log(eat('bananas '));
